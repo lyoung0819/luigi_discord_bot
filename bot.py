abd3b38cff1a742d30439322bc1f8205efed0e88
lyoung0819 <159396936+lyoung0819@users.noreply.github.com>
2024-07-18 19:49:43 -0500

    git ignore, run_disc_bot() added

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+import responses
+from . import token
+
+async def send_message(message, user_message, is_private):
+    try:
+        response = response.handle_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    client = discord.Client(intents=discord.Intents.default())
+
+    @client.event
+    async def on_ready():
+        print(f"{client.user} is now running!")
+
+    client.run(token)
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import bot
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('MY_TOKEN')
+
 if __name__ == '__main__':
     #run the bot
-    pass
+    bot.run_discord_bot()
